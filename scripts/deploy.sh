ad98d4882f4cf250391f5d2e2a99a11e367fca86
zero2080 <zero2080@naver.com>
2021-10-28 00:54:50 +0900

    배포 자동화 최종

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ REPOSITORY=/home/ec2-user/app/step2
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.war  /home/apache-tomcat-8.5.72/webapps/
+cp $REPOSITORY/zip/*.war  ~/apache-tomcat-8.5.72/webapps/
 
 echo "> 새 어플리케이션 배포"
